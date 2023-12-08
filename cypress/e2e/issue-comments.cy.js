45e5dd749fa59c64408d21f9c3d6beb47df3b6bb
1Jabka1 <60707808+1Jabka1@users.noreply.github.com>
2023-12-08 17:57:58 +0200

    Update issue-comments.cy.js

--- cypress/e2e/issue-comments.cy.js
+++ cypress/e2e/issue-comments.cy.js
@@ -68,4 +68,60 @@ describe('Issue comments creating, editing and deleting', () => {
             .find('[data-testid="issue-comment"]')
             .should('not.exist');
     });
+
+    it.only('Automation test: Adding, updating, and deleting comments', ()  => {
+        const comment = 'Lorem ipsum dolor sit amet';
+        const moddedComment = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+
+
+        getIssueDetailsModal().within(() => {
+            cy.contains('Add a comment...')
+                .click();
+
+            cy.get('textarea[placeholder="Add a comment..."]').type(comment);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.contains('Add a comment...').should('exist');
+            cy.get('[data-testid="issue-comment"]').should('contain', comment);
+        });
+
+        getIssueDetailsModal().within(() => {
+            cy.get('[data-testid="issue-comment"]')
+                .first()
+                .contains('Edit')
+                .click()
+                .should('not.exist');
+
+            cy.get('textarea[placeholder="Add a comment..."]')
+                .should('contain', comment)
+                .clear()
+                .type(moddedComment);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.get('[data-testid="issue-comment"]')
+                .should('contain', 'Edit')
+                .and('contain', comment);
+        });
+
+            getIssueDetailsModal()
+                .find('[data-testid="issue-comment"]')
+                .contains('Delete')
+                .click();
+
+            cy.get('[data-testid="modal:confirm"]')
+                .contains('button', 'Delete comment')
+                .click()
+                .should('not.exist');
+
+            getIssueDetailsModal()
+                .find('[data-testid="issue-comment"]')
+                .contains(moddedComment)
+                .should('not.exist');
+        });
 });
